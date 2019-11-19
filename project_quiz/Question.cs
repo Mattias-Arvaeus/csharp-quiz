25917e33074799aa4be9189e499e6664d153bc81
Mattias Arvaeus <matte.arve@gmail.com>
2019-11-19 09:48:39 +0100

    Added files

---
+++ project_quiz/Question.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_quiz
+{
+    class Question
+    {
+        private string land;
+        private string answer;
+
+        public Question(string land, string answer)
+        {
+            this.land = land;
+            this.answer = answer;
+        }
+
+        public bool CheckAnswer(string userAnswer)
+        {
+            if (userAnswer == this.answer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Land
+        {
+            get
+            {
+                return land;
+            }
+        }
+
+        public string Answer
+        {
+            get
+            {
+                return answer;
+            }
+        }
+    }
+}
---
+++ project_quiz/UserInterface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_quiz
+{
+    class UserInterface
+    {
+        private string answer;
+        private int questionCount;
+        private int correctCount;
+
+        public UserInterface()
+        {
+            this.questionCount = 0;
+            this.correctCount = 0;
+        }
+
+        public void DisplayQuestion(Question question)
+        {
+
+        }
+
+        public void DisplayCounter()
+        {
+
+        }
+    }
+}
